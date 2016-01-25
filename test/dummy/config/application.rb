82968ff7a1770b388dcb2888996dfbc5e748e341
Callum Hogg <bananaCypher@gmail.com>
2016-01-25 22:55:26 +0000

    Added a Engine manually to test adding controllers
    I manually set up a controller and route for Articles with the view to
    create a rake task to do it for me in the future.

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "falsememories"
+require "false_memories"
 
 module Dummy
   class Application < Rails::Application
