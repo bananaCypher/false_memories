0c2e0b08ec006142aebb99a18fadfc13b310bb73
Callum Hogg <bananaCypher@gmail.com>
2016-01-26 22:09:56 +0000

    Updated the generator to search for applicable models

--- lib/generators/false_memories/false_memories_generator.rb
+++ lib/generators/false_memories/false_memories_generator.rb
@@ -1,25 +1,39 @@
 class FalseMemoriesGenerator < Rails::Generators::Base
     source_root File.expand_path('../templates', __FILE__)
-    argument :model_name, :type => :string
 
+    def start_generating
+        Rails.application.eager_load!
+        models = ActiveRecord::Base.descendants
+        models.each do |model|
+            begin
+                model.falsememories
+            rescue
+                next
+            end
+            puts "Setting up FalseMemory for #{model.name}"
+            create_controller(model)
+            create_routes(model)
+        end
+    end
+
+    
+    private
     def create_controller
+        @controller_name = controller_name(model)
+        @model = model.to_s
         template "controller.rb.erb", "app/controllers/false_memories/#{controller_name.underscore}.rb"
     end
 
     def create_routes
-        route "get '#{route_reference}/:id/memories' => 'false_memories/#{route_reference}#memories'"
+        @route_reference = route_reference(model)
+        route "get '#{@route_reference}/:id/memories' => 'false_memories/#{@route_reference}#memories'"
     end
 
-    private
-    def route_reference
+    def route_reference(model)
         model.to_s.tableize.underscore
     end
 
-    def controller_name
+    def controller_name(model)
         "#{model.to_s.tableize.capitalize}Controller"
-    end
-
-    def model
-        eval model_name.capitalize
     end
 end
